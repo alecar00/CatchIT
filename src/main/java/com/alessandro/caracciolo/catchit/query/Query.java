ad7a68b23d8d35d5a8aa8302adcbd8bf4d724cb0
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-05-24 14:32:08 +0200

    add id_rider and permit_ztl parameters on select

--- src/main/java/com/alessandro/caracciolo/catchit/query/Query.java
+++ src/main/java/com/alessandro/caracciolo/catchit/query/Query.java
@@ -3,9 +3,9 @@ package com.alessandro.caracciolo.catchit.query;
 public class Query {
     private Query(){}
 
-    public static final String GET_PENDING_ORDERS = "SELECT * FROM Orders WHERE status = 1";
+    public static final String GET_PENDING_ORDERS = "SELECT * FROM Orders WHERE status = ?";
 
-    public static final String GET_AVAILABLE_RIDERS = "SELECT r.name " +
+    public static final String GET_AVAILABLE_RIDERS = "SELECT r.id_rider, r.name, r.permit_ztl " +
             "FROM Rider r " +
             "LEFT JOIN Orders o ON r.id_rider = o.id_rider " +
             "AND o.delivery_time = ? " +
