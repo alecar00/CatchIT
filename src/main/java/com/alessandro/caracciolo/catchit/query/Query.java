1d7a1b5407c14b5789289f17b6cee1baa4b23975
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-05-24 11:37:45 +0200

    add get_available_riders query

--- src/main/java/com/alessandro/caracciolo/catchit/query/Query.java
+++ src/main/java/com/alessandro/caracciolo/catchit/query/Query.java
@@ -5,4 +5,11 @@ public class Query {
 
     public static final String GET_PENDING_ORDERS = "SELECT * FROM Orders WHERE status = 1";
 
+    public static final String GET_AVAILABLE_RIDERS = "SELECT r.name " +
+            "FROM Rider r " +
+            "LEFT JOIN Orders o ON r.id_rider = o.id_rider " +
+            "AND o.delivery_time = ? " +
+            "AND o.status IN ('ASSIGNED', 'IN_DELIVERY') " +
+            "WHERE o.id_order IS NULL; ";
+
 }
