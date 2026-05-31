db51a2f3ad23a63774020736e78ce5fa4d30529d
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-05-31 11:50:58 +0200

    add initialize, updateOrdersList, printOrder and printTitle method

--- src/main/java/com/alessandro/caracciolo/catchit/view/cli/RestaurantViewCLI.java
+++ src/main/java/com/alessandro/caracciolo/catchit/view/cli/RestaurantViewCLI.java
@@ -1,4 +1,37 @@
 package com.alessandro.caracciolo.catchit.view.cli;
 
+import com.alessandro.caracciolo.catchit.bean.OrderBean;
+import com.alessandro.caracciolo.catchit.controller.ProcessOrderController;
+
+import java.util.List;
+
 public class RestaurantViewCLI {
+    private ProcessOrderController processOrderController;
+
+    public void initialize() {
+        this.processOrderController = new ProcessOrderController();
+
+        List<OrderBean> orderBeans = processOrderController.discoverPendingOrders();
+        updateOrdersList(orderBeans);
+    }
+
+    private void updateOrdersList(List<OrderBean> orderBeans) {
+        printTitle("Pending Orders");
+        for (OrderBean orderBean : orderBeans) {
+            printOrder(orderBean);
+        }
+    }
+
+    private void printOrder(OrderBean orderBean) {
+        System.out.println("- Order ID: " + orderBean.getIdOrder());
+        System.out.println("-- Consumer: " + orderBean.getConsumer());
+        System.out.println("-- Address: " + orderBean.getAddress());
+        System.out.println("-- Time: " + orderBean.getTime());
+    }
+
+    private void printTitle(String title) {
+        System.out.println("------------------------------------");
+        System.out.println("-- " + title);
+        System.out.println("------------------------------------");
+    }
 }
