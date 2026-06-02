4773d2efe2376dbbb478d7a556d834a390d69e02
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-02 14:22:40 +0200

    add dependency for delivery use case and gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,16 @@ module com.alessandro.caracciolo.catchit {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires java.sql;
+    requires java.desktop;
+    requires com.sothawo.mapjfx;
+    requires com.google.gson;
     opens com.alessandro.caracciolo.catchit to javafx.fxml;
     exports com.alessandro.caracciolo.catchit;
     exports com.alessandro.caracciolo.catchit.controller;
     exports com.alessandro.caracciolo.catchit.bean;
+    exports com.alessandro.caracciolo.catchit.model;
+    exports com.alessandro.caracciolo.catchit.exceptions;
     opens com.alessandro.caracciolo.catchit.view.controller to javafx.fxml;
+    opens com.alessandro.caracciolo.catchit.model to com.google.gson;
+    opens com.alessandro.caracciolo.catchit.Observer to com.google.gson;
 }
