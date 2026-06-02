e48b0fa607b89bd0678a750a2378c8fd68b81ab0
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-02 14:06:54 +0200

    add a gson provider class

---
+++ src/main/java/com/alessandro/caracciolo/catchit/utils/GsonProvider.java
@@ -0,0 +1,40 @@
+package com.alessandro.caracciolo.catchit.utils;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.sql.Time;
+
+public class GsonProvider {
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(Time.class, new TypeAdapter<Time>() {
+                @Override
+                public void write(JsonWriter out, Time value) throws IOException {
+                    if (value == null) {
+                        out.nullValue();
+                    } else {
+                        out.value(value.toString());
+                    }
+                }
+
+                @Override
+                public Time read(JsonReader in) throws IOException {
+                    if (in.peek() == com.google.gson.stream.JsonToken.NULL) {
+                        in.nextNull();
+                        return null;
+                    }
+                    return Time.valueOf(in.nextString());
+                }
+            })
+            .setPrettyPrinting()
+            .create();
+
+    public static Gson getGson() {
+        return gson;
+    }
+}
