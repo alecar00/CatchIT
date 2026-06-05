adb53aa4e8a3f1c75713f8bb579da553e8396347
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-05 22:39:27 +0200

    implement RiderDAOMemory

--- src/main/java/com/alessandro/caracciolo/catchit/dao/memory/RiderDAOMemory.java
+++ src/main/java/com/alessandro/caracciolo/catchit/dao/memory/RiderDAOMemory.java
@@ -5,9 +5,18 @@ import com.alessandro.caracciolo.catchit.model.Order;
 import com.alessandro.caracciolo.catchit.model.Rider;
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.alessandro.caracciolo.catchit.dao.memory.OrderDAOMemory.orders;
+
 public class RiderDAOMemory implements RiderDAO {
+    private static List<Rider> riders = new ArrayList<>();
+
+    static{
+        riders.add(new Rider("1", "Pasquale Bianchi", true));
+    }
+
     @Override
     public void saveRider(Rider rider) {
         //not implemented
@@ -15,16 +24,25 @@ public class RiderDAOMemory implements RiderDAO {
 
     @Override
     public List<Rider> getAllRiders() {
-        return List.of();
+        return riders;
     }
 
     @Override
     public Rider getRiderById(String id) {
+        for (Rider rider : riders) {
+            if (rider.getIdRider().equals(id)) return rider;
+        }
         return null;
     }
 
     @Override
-    public List<Rider> getAvailableRiders(Order order, Time time) {
-        return List.of();
+    public List<Rider> getAvailableRiders(Order selectedOrder, Time time) {
+        List<Rider> availableRiders = riders;
+        for (Order order : orders) {
+            if (order.getTime().equals(time)) {
+                availableRiders.remove(order.getRider());
+            }
+        }
+        return availableRiders;
     }
 }
