1c7f06fcf154a0b7c0032f2010d85af4122fb5a9
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-06 14:50:37 +0200

    setOrderCompleted throws BusinessException

--- src/main/java/com/alessandro/caracciolo/catchit/controller/DeliveryController.java
+++ src/main/java/com/alessandro/caracciolo/catchit/controller/DeliveryController.java
@@ -2,6 +2,7 @@ package com.alessandro.caracciolo.catchit.controller;
 
 import com.alessandro.caracciolo.catchit.dao.DAOFactory;
 import com.alessandro.caracciolo.catchit.dao.OrderDAO;
+import com.alessandro.caracciolo.catchit.exceptions.BusinessException;
 import com.alessandro.caracciolo.catchit.exceptions.DAOException;
 import com.alessandro.caracciolo.catchit.model.Rider;
 
@@ -10,21 +11,16 @@ import java.util.logging.Logger;
 public class DeliveryController {
     private static final Logger log = Logger.getLogger(DeliveryController.class.getName());
 
-    public void initialize(){
-        //not implemented
+    public void initialize() {
     }
 
-    private void showOrdersRider(Rider rider){
-        //not implemented
+    private void showOrdersRider(Rider rider) {
     }
 
-
-
-    public void startDelivery(String idOrder){
-        //not implemented
+    public void startDelivery(String idOrder) {
     }
 
-    public void setOrderCompleted(String idOrder) throws DAOException {
+    public void setOrderCompleted(String idOrder) throws DAOException, BusinessException {
         OrderDAO orderDAO = DAOFactory.getDAOFactory().createOrderDAO();
         orderDAO.setOrderCompleted(idOrder);
     }
