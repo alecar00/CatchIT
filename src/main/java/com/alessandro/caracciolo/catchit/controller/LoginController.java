2a677baaba33267a69a9643c6590d8c624833168
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-06 14:50:57 +0200

    implement login logic

--- src/main/java/com/alessandro/caracciolo/catchit/controller/LoginController.java
+++ src/main/java/com/alessandro/caracciolo/catchit/controller/LoginController.java
@@ -1,17 +1,29 @@
 package com.alessandro.caracciolo.catchit.controller;
 
 import com.alessandro.caracciolo.catchit.bean.UserBean;
+import com.alessandro.caracciolo.catchit.dao.DAOFactory;
+import com.alessandro.caracciolo.catchit.dao.UserDAO;
 import com.alessandro.caracciolo.catchit.exceptions.BusinessException;
 import com.alessandro.caracciolo.catchit.exceptions.DAOException;
-
-import java.util.logging.Logger;
+import com.alessandro.caracciolo.catchit.model.User;
 
 public class LoginController {
-    private static final Logger log = Logger.getLogger(LoginController.class.getName());
 
     public int verifyCredentials(UserBean userBean) throws DAOException, BusinessException {
-        //not implemented
-        return 1;// 1 = restaurant, 2 = rider
-    }
 
+        UserDAO userDAO = DAOFactory.getDAOFactory().createUserDAO();
+
+        User user = userDAO.getUserByUsername(userBean.getUsername());
+
+        if (user == null) {
+            throw new BusinessException("Username not found in the system.");
+        }
+
+        if (!user.getPassword().equals(userBean.getPassword())) {
+            throw new BusinessException("Incorrect password.");
+        }
+
+        //Se tutto è corretto, restituiamo l'ID del ruolo (1=Ristorante, 2=Rider)
+        return user.getRole().getId();
+    }
 }
