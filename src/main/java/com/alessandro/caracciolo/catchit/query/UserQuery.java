de64d3f5ebff9b387e7a7e0490e2703c3f6c993f
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-27 22:02:13 +0200

    add insert users query

--- src/main/java/com/alessandro/caracciolo/catchit/query/UserQuery.java
+++ src/main/java/com/alessandro/caracciolo/catchit/query/UserQuery.java
@@ -1,15 +1,38 @@
 package com.alessandro.caracciolo.catchit.query;
 
+import com.alessandro.caracciolo.catchit.exceptions.DAOException;
+import com.alessandro.caracciolo.catchit.exceptions.InvalidRegistrationException;
 import com.alessandro.caracciolo.catchit.model.User;
 
 import java.sql.*;
 
 public class UserQuery {
-    private UserQuery(){};
+    private UserQuery(){}
 
     public static ResultSet getUserByUsername(Connection conn, String username) throws SQLException {
         PreparedStatement stmt = conn.prepareStatement(Query.GET_USER_BY_USERNAME);
         stmt.setString(1, username);
         return stmt.executeQuery();
+    }
+
+    public static int insertRestaurant(Connection conn, User user) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(Query.INSERT_RESTAURANT);
+        stmt.setString(1, user.getUsername());
+        stmt.setString(2, user.getPassword());
+        return stmt.executeUpdate();
+    }
+
+    public static int insertRider(Connection conn, User user) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(Query.INSERT_RIDER);
+        stmt.setString(1, user.getUsername());
+        stmt.setString(2, user.getPassword());
+        return stmt.executeUpdate();
+    }
+
+    public static boolean checkUsername(Connection conn, String username) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(Query.CHECK_USERNAME);
+        stmt.setString(1, username);
+        ResultSet rs = stmt.executeQuery();
+        return rs.next();
     }
 }
