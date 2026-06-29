81cb53dd9c589fe050bccdeedaeaff1c8015b9cd
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-06-29 16:28:47 +0200

    add general error alert

--- src/main/java/com/alessandro/caracciolo/catchit/utils/AlertHandler.java
+++ src/main/java/com/alessandro/caracciolo/catchit/utils/AlertHandler.java
@@ -50,4 +50,12 @@ public class AlertHandler {
         alert.setContentText(e.getMessage());
         alert.showAndWait();
     }
+
+    public static void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
