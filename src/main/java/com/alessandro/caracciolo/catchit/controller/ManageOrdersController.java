e2f8f9d1c39a0d53c60136134286d4e1d5b49b75
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-07-12 13:34:10 +0200

    bug fix for riderId

--- src/main/java/com/alessandro/caracciolo/catchit/controller/ManageOrdersController.java
+++ src/main/java/com/alessandro/caracciolo/catchit/controller/ManageOrdersController.java
@@ -1,6 +1,7 @@
 package com.alessandro.caracciolo.catchit.controller;
 
 import com.alessandro.caracciolo.catchit.bean.OrderBean;
+import com.alessandro.caracciolo.catchit.bean.RiderBean;
 import com.alessandro.caracciolo.catchit.dao.DAOFactory;
 import com.alessandro.caracciolo.catchit.dao.OrderDAO;
 import com.alessandro.caracciolo.catchit.model.Order;
@@ -33,12 +34,13 @@ public class ManageOrdersController {
         }
 
         for (Order rs: orders){
-            var orderBean = new OrderBean(rs.getIdOrder(),
-                    rs.getAddress(),
-                    rs.getCostumer(),
-                    rs.getTelNumber(),
-                    rs.getTime(),
-                    rs.getStatus());
+            OrderBean orderBean;
+            if (rs.getRider() != null) {
+                RiderBean rBean = new RiderBean(rs.getRider().getIdRider(), rs.getRider().getName());
+                orderBean = new OrderBean(rs.getIdOrder(), rs.getAddress(), rs.getCostumer(), rs.getTelNumber(), rBean, rs.getTime(), rs.getStatus());
+            } else {
+                orderBean = new OrderBean(rs.getIdOrder(), rs.getAddress(), rs.getCostumer(), rs.getTelNumber(), rs.getTime(), rs.getStatus());
+            }
             ordersBean.add(orderBean);
         }
         logger.info(() -> "Found " + ordersBean.size() + " orders");
