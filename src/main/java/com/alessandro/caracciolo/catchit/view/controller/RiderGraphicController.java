b3c6cd0a60a8f335b8a270699ed0d92d156bc912
alecar00 <alessandro.caracciolo.00@gmail.com>
2026-07-12 12:09:43 +0200

    use switch scene

--- src/main/java/com/alessandro/caracciolo/catchit/view/controller/RiderGraphicController.java
+++ src/main/java/com/alessandro/caracciolo/catchit/view/controller/RiderGraphicController.java
@@ -1,6 +1,5 @@
 package com.alessandro.caracciolo.catchit.view.controller;
 
-import com.alessandro.caracciolo.catchit.Main;
 import com.alessandro.caracciolo.catchit.bean.OrderBean;
 import com.alessandro.caracciolo.catchit.controller.DeliveryController;
 import com.alessandro.caracciolo.catchit.exceptions.BusinessException;
@@ -9,19 +8,14 @@ import com.alessandro.caracciolo.catchit.singleton.Configs;
 import com.alessandro.caracciolo.catchit.utils.AlertHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -103,25 +97,13 @@ public class RiderGraphicController {
             deliveryController.startDelivery(order.getIdOrder(), riderId);
             AlertHandler.showSuccess("Delivery Started", "Order #" + order.getIdOrder() + " is now in delivery.");
 
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/view/DeliveryView.fxml"));
-            Parent rootOrdini = loader.load();
-
-            DeliveryGraphicController deliveryController = loader.getController();
-            deliveryController.initData(order.getIdOrder());
-
-            Stage stageAttuale = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-            stageAttuale.setScene(new Scene(rootOrdini));
-            stageAttuale.show();
+            SceneSwitcher.switchToDelivery((Node) event.getSource());
 
         } catch (DAOException e) {
             logger.severe("Error starting delivery: " + e.getMessage());
             AlertHandler.showDAOError(e);
         } catch (BusinessException e) {
             AlertHandler.showBusinessError(e);
-        } catch (IOException e){
-            logger.severe("Fatal error loading DeliveryView.fxml: " + e.getMessage());
-            AlertHandler.showDAOError(new DAOException("Critical UI Error: Unable to load view."));
         }
     }
 
